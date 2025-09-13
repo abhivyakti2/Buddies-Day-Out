da4bb5b0aa00ef832e2641b0a612c573d5cae32d
Abhivyakti <abhivyakti002btit23@igdtuw.ac.in>
2025-09-13 14:19:16 +0530

    css-issue

---
+++ buddies-day-out/tailwind.config.js
@@ -0,0 +1,12 @@
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',                 // your main HTML file
+    './src/**/*.{js,ts,jsx,tsx}'   // all your React components
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
